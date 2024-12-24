7d305cc7fa7247cb8a812ab32f66ece391c8141c
kaivanov8 <kosiv6799@gmail.com>
2024-12-24 17:52:57 +0700

    Add files via upload

---
+++ module_9_2.py
@@ -0,0 +1,12 @@
+# cписковые, словарные сборки
+
+first_string = ['Elon','Musk','Programmer','Monitors','Variable']
+second_string = ['Task','Git','Comprehension','Java','Computer','Assembler']
+
+first_result = [len(x) for x in first_string if len(x) > 5]
+second_result = [(x,y) for x in first_string for y in second_string if len(x) == len(y)]
+third_result = {x:len(x) for x in first_string + second_string if not(len(x) % 2)}
+
+print(first_result)
+print(second_result)
+print(third_result)
